3e7f8cf7e2243d21b247502f250162ad61c12449
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-08-05 21:09:43 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,38 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-    addSkillBtn = document.querySelector('.addskill')
-    const skillModal = document.querySelector('.skillmodal')
+  const addSkillBtn = document.querySelector(".addskill");
+  const skillModal = document.querySelector(".skillmodal");
+
+  const skills = document.querySelector('.skills')
 
 
-    ///display modal
-    addSkillBtn.addEventListener("click", () => {
-        skillModal.showModal()
-    })
-    //hidemodal
-    skillModal.addEventListener('click', (event) => {
-        if (event.target === event.currentTarget) {
-            skillModal.close()
-        }
-    })
- 
+  //functions
+  //create the list item element
 
+  const createListItem = (text) => {
+    const li = document.createElement('li')
+    li.innerHTML = `<span>${text}</span><button>&times;</button>`
 
-
-
+    return li
+  }
+//event listner
 
 
 
+  ///display modal
+  addSkillBtn.addEventListener("click", () => {
+    skillModal.showModal();
+  });
+  //hidemodal
+  skillModal.addEventListener("click", (event) => {
+    if (event.target === event.currentTarget) {
+      skillModal.close();
+    }
+  });
 
 
 
+  ///handle form submit
+
+  skillModal.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
 
 
+    const skill = skillModal.querySelector('#skill').ariaValueMax.trim()
 
-
-
-
-
-
-
-
-
-})
+    if(skill) {
+        console.log(skill)
+    }
+  })
+});
